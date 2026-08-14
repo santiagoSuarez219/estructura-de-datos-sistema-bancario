d59d72ca0fe9d72ecfc46129a810a66e7e205469
santiagoSuarez219 <santiago8628@gmail.com>
2026-08-14 14:08:26 -0500

    agrega modificadores de acceso a la clase Cliente y getters y setters

--- src/Cliente.java
+++ src/Cliente.java
@@ -1,15 +1,25 @@
 public class Cliente {
 
-    String nombre;
-    String cedula;
-    String telefono;
-    int edad;
+    private String nombre; // Obligatorio
+    private String cedula; // Obligatorio
+    private String telefono; // Opcional
+    private int edad; // Opcional
+    private int saldo; // Automatizar
 
-    public Cliente(String nombre, String cedula, String telefono, int edad) {
-        this.nombre = nombre;
-        this.cedula = cedula;
+    public Cliente(String nombre, String cedula, String telefono, int edad, int saldo) {
+        this(nombre, cedula);
         this.telefono = telefono;
         this.edad = edad;
+    }
+
+    public Cliente(String nombre, String cedula) {
+        this.nombre = nombre;
+        this.cedula = cedula;
+        this.saldo = 0;
+    }
+
+    public Cliente(String nombre) {
+        this.nombre = nombre;
     }
 
     public void mostrarInfo() {
@@ -17,6 +27,49 @@ public class Cliente {
         System.out.println("Cedula: " + this.cedula);
         System.out.println("Telefono: " + this.telefono);
         System.out.println("Edad: " + this.edad);
+    }
+
+    public int getSaldo() {
+        return this.saldo;
+    }
+
+    public void setSaldo(int saldo) {
+        if (saldo < 0) {
+            System.out.println("El saldo no puede ser negativo");
+        } else {
+            this.saldo = saldo;
+            System.out.println("El saldo ha sido actualizado correctamente");
+        }
+    }
+
+    // get: nombre
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    // set : nombre
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getCedula() {
+        return cedula;
     }
 
 }
