f8e23e5b281c4aa511663d122ac5787e9ca717b6
santiagoSuarez219 <santiago8628@gmail.com>
2026-08-14 14:09:58 -0500

    crea objetos del tipo cliente y utiliza los metodos set, get y sobrecarga del metodo constructor

--- src/App.java
+++ src/App.java
@@ -1,5 +1,21 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Cliente maria = new Cliente("Maria", "123", "3456", 27, 20000);
+        // System.out.println(maria.telefono);
+        System.out.println(maria.getSaldo());
+        maria.setSaldo(-30000);
+        System.out.println(maria.getSaldo());
+
+        Cliente carlos = new Cliente("Carlos", "567");
+        carlos.setTelefono("345545");
+        carlos.setEdad(45);
+        System.out.println(carlos.getSaldo());
+        System.out.println(carlos.getEdad());
+
+        Cliente ana = new Cliente("Ana");
+
+        Cliente juan = new Cliente("Juan", "4567", "23345", 21, 30000);
+        System.out.println(juan.getSaldo());
+
     }
 }
