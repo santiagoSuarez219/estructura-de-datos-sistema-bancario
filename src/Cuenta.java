47a661b7948e6dcdc72158a1169e7acbce429e0b
santiagoSuarez219 <santiago8628@gmail.com>
2026-08-14 14:08:51 -0500

    agrega modificadores de acceso a la clase Cuenta y getters y setters

--- src/Cuenta.java
+++ src/Cuenta.java
@@ -1,9 +1,9 @@
 public class Cuenta {
 
-    String numeroCuenta;
-    String tipoCuenta;
-    double saldo;
-    String titular;
+    private String numeroCuenta;
+    private String tipoCuenta;
+    private double saldo;
+    private String titular;
 
     public Cuenta(String numeroCuenta, String tipoCuenta, double saldo, String titular) {
         this.numeroCuenta = numeroCuenta;
@@ -17,6 +17,38 @@ public class Cuenta {
         System.out.println("Tipo de cuenta: " + this.tipoCuenta);
         System.out.println("Saldo: " + this.saldo);
         System.out.println("Titular: " + this.titular);
+    }
+
+    public String getNumeroCuenta() {
+        return numeroCuenta;
+    }
+
+    public void setNumeroCuenta(String numeroCuenta) {
+        this.numeroCuenta = numeroCuenta;
+    }
+
+    public String getTipoCuenta() {
+        return tipoCuenta;
+    }
+
+    public void setTipoCuenta(String tipoCuenta) {
+        this.tipoCuenta = tipoCuenta;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public String getTitular() {
+        return titular;
+    }
+
+    public void setTitular(String titular) {
+        this.titular = titular;
     }
 
 }
