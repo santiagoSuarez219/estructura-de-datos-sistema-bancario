2d6786e6ce85e23bb059828dedd3bf3a98279af4
santiagoSuarez219 <santiago8628@gmail.com>
2026-08-25 14:05:56 -0500

    instacia objetos de la clase tarjeta debito y credito

--- src/App.java
+++ src/App.java
@@ -1,21 +1,16 @@
+import com.itm.sistemabancario.models.TarjetaCredito;
+import com.itm.sistemabancario.models.TarjetaDebito;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        Cliente maria = new Cliente("Maria", "123", "3456", 27, 20000);
-        // System.out.println(maria.telefono);
-        System.out.println(maria.getSaldo());
-        maria.setSaldo(-30000);
-        System.out.println(maria.getSaldo());
+        TarjetaDebito visa01 = new TarjetaDebito(123, "Visa", "Santiago");
+        TarjetaCredito mastercard02 = new TarjetaCredito(456, "MasterCard", "Maria");
 
-        Cliente carlos = new Cliente("Carlos", "567");
-        carlos.setTelefono("345545");
-        carlos.setEdad(45);
-        System.out.println(carlos.getSaldo());
-        System.out.println(carlos.getEdad());
+        double saldo = visa01.mostrarSaldo();
+        double saldo2 = mastercard02.mostrarCupo();
 
-        Cliente ana = new Cliente("Ana");
-
-        Cliente juan = new Cliente("Juan", "4567", "23345", 21, 30000);
-        System.out.println(juan.getSaldo());
+        visa01.pagar(20000.0);
+        mastercard02.pagar(20000.0);
 
     }
 }
