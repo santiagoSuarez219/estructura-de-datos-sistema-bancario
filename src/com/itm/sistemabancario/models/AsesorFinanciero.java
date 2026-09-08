a4a5dc31db82ee5cb9821c01d21ad26f9dd0647b
santiagoSuarez219 <santiago8628@gmail.com>
2026-09-08 08:49:31 -0500

    crea la clase AsesorFinanciero con sus atributos, contructores y metodos

---
+++ src/com/itm/sistemabancario/models/AsesorFinanciero.java
@@ -0,0 +1,17 @@
+package com.itm.sistemabancario.models;
+
+public class AsesorFinanciero {
+    private String nombre;
+
+    public AsesorFinanciero(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
