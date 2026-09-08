186d76012d735fcabdaedfd5a75f0dc443de1271
santiagoSuarez219 <santiago8628@gmail.com>
2026-09-08 09:01:12 -0500

    instancia objetos del tipo cuenta para utilizar el metodo depositar

--- src/App.java
+++ src/App.java
@@ -1,16 +1,9 @@
-import com.itm.sistemabancario.models.TarjetaCredito;
-import com.itm.sistemabancario.models.TarjetaDebito;
+import com.itm.sistemabancario.models.CuentaAhorros;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        TarjetaDebito visa01 = new TarjetaDebito(123, "Visa", "Santiago");
-        TarjetaCredito mastercard02 = new TarjetaCredito(456, "MasterCard", "Maria");
-
-        double saldo = visa01.mostrarSaldo();
-        double saldo2 = mastercard02.mostrarCupo();
-
-        visa01.pagar(20000.0);
-        mastercard02.pagar(20000.0);
+        CuentaAhorros cuentaAhorros = new CuentaAhorros("123", 0.1);
+        cuentaAhorros.depositar(20000.0);
 
     }
 }
