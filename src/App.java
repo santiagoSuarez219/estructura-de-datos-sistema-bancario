dbe6afcf243756815291d4daa49b52bee72b7462
santiagoSuarez219 <santiago8628@gmail.com>
2026-09-15 09:20:02 -0500

    instancia el menu dentro de App.java

--- src/App.java
+++ src/App.java
@@ -1,9 +1,7 @@
-import com.itm.sistemabancario.models.CuentaAhorros;
+import com.itm.sistemabancario.view.MenuView;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        CuentaAhorros cuentaAhorros = new CuentaAhorros("123", 0.1);
-        cuentaAhorros.depositar(20000.0);
-
+        new MenuView().iniciar();
     }
 }
