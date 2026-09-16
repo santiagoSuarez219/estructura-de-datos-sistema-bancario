61ee1a2a7870bfaa6eba981bee03c9624b770004
santiagoSuarez219 <santiago8628@gmail.com>
2026-09-16 11:46:35 -0500

    simplifica la creación de clientes y mejora la legibilidad del código

--- src/com/itm/sistemabancario/services/ClienteService.java
+++ src/com/itm/sistemabancario/services/ClienteService.java
@@ -3,18 +3,14 @@ package com.itm.sistemabancario.services;
 import java.time.LocalDate;
 
 import com.itm.sistemabancario.models.Cliente;
-import com.itm.sistemabancario.models.Cuenta;
-import com.itm.sistemabancario.models.CuentaAhorros;
-import com.itm.sistemabancario.models.CuentaCorriente;
 import com.itm.sistemabancario.structures.ListaSimple;
-import com.itm.sistemabancario.structures.Nodo;
 
 public class ClienteService {
-    private ListaSimple<Cliente> clientes = new ListaSimple<>();
+    private ListaSimple<Cliente> clientes = new ListaSimple<>(); // Lista de clientes del sistema
 
     public Cliente crearCliente(String identificacion, String nombre, String telefono, String direccion) {
-        Cliente cliente = new Cliente(identificacion, nombre, telefono, direccion);
-        clientes.insertarFinal(cliente);
+        Cliente cliente = new Cliente(identificacion, nombre, telefono, direccion); // Crea el objeto de tipo Cliente
+        clientes.crear(0, cliente);
         return cliente;
     }
 
@@ -26,13 +22,13 @@ public class ClienteService {
     // return null;
     // }
 
-    // public Cliente buscarPorIndice(int indice) {
-    // // clienteEncontrado = buscarPorIndice();
-    // if (clienteEncontrado.getIdentificacion().equals(identificacion)) {
-    // return cliente;
-    // }
-    // return null;
-    // }
+    public Cliente buscarPorIndice(int indice) {
+        Cliente clienteEncontrado = clientes.buscarPorIndice(indice);
+        if (clienteEncontrado == null) {
+            return null;
+        }
+        return clienteEncontrado;
+    }
 
     // public void recorrerLista() {
     // Nodo<Cliente> actual = clientes.getHead();
