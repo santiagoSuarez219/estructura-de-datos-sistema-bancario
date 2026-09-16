1c04f9b0e3b7a43a8b707bd80991702e5f669794
santiagoSuarez219 <santiago8628@gmail.com>
2026-09-16 08:15:13 -0500

    implementa métodos buscarPorIndice y buscarPorValor en ListaSimple

--- src/com/itm/sistemabancario/structures/ListaSimple.java
+++ src/com/itm/sistemabancario/structures/ListaSimple.java
@@ -68,30 +68,43 @@ public class ListaSimple<T> implements OperacionesEstructuras<T> {
     }
 
     @Override
-    public void buscarPorIndice(int index) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'buscarPorIndice'");
+    public T buscarPorIndice(int indice) {
+        if (indice < 0 || indice >= tamano) {
+            throw new IndexOutOfBoundsException("Indice fuera de rango: " + indice);
+        }
+        Nodo<T> actual = head;
+        for (int i = 0; i < indice; i++) {
+            actual = actual.getSiguiente();
+        }
+        return actual.getDato();
     }
 
     @Override
-    public void buscarPorValor(T dato) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'buscarPorValor'");
+    public T buscarPorValor(T dato) {
+        Nodo<T> actual = head;
+        while (actual != null) {
+            if (actual.getDato().equals(dato)) {
+                return actual.getDato();
+            }
+            actual = actual.getSiguiente();
+        }
+        System.out.println("Dato no encontrado");
+        return null;
+    }
+
+    public Nodo<T> getHead() {
+        return head;
     }
 
     @Override
-    public void actualizar(int index, T dato) {
+    public void actualizar(int indice, T dato) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'actualizar'");
     }
 
     @Override
-    public void eliminar(int index) {
+    public boolean eliminar(T dato) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'eliminar'");
-    }
-
-    public Nodo<T> getHead() {
-        return head;
     }
 }
