788638d00e30c639e55acce938a27e8173906b9c
santiagoSuarez219 <santiago8628@gmail.com>
2026-09-16 08:11:51 -0500

    implementa métodos de búsqueda, recorrido, actualización y eliminacio. en ClienteService

--- src/com/itm/sistemabancario/services/ClienteService.java
+++ src/com/itm/sistemabancario/services/ClienteService.java
@@ -1,11 +1,6 @@
 package com.itm.sistemabancario.services;
 
-import java.time.LocalDate;
-
 import com.itm.sistemabancario.models.Cliente;
-import com.itm.sistemabancario.models.Cuenta;
-import com.itm.sistemabancario.models.CuentaAhorros;
-import com.itm.sistemabancario.models.CuentaCorriente;
 import com.itm.sistemabancario.structures.ListaSimple;
 import com.itm.sistemabancario.structures.Nodo;
 
@@ -18,30 +13,77 @@ public class ClienteService {
         return cliente;
     }
 
-    // public Cliente buscarPorIdentificacion(String identificacion) {
-    // // clienteEncontrado = buscarPorValor();
-    // if (clienteEncontrado.getIdentificacion().equals(identificacion)) {
-    // return cliente;
-    // }
-    // return null;
-    // }
+    public Cliente buscarPorIdentificacion(String identificacion) {
+        Nodo<Cliente> clienteActual = clientes.getHead();
+        while (clienteActual != null) {
+            if (clienteActual.getDato().getIdentificacion().equals(identificacion)) {
+                return clienteActual.getDato();
+            }
+            clienteActual = clienteActual.getSiguiente();
+        }
+        System.out.println("Dato no encontrado");
+        return null;
+    }
 
-    // public Cliente buscarPorIndice(int indice) {
-    // // clienteEncontrado = buscarPorIndice();
-    // if (clienteEncontrado.getIdentificacion().equals(identificacion)) {
-    // return cliente;
-    // }
-    // return null;
-    // }
+    public Cliente buscarPorIndice(int indice) {
+        Cliente clienteEncontrado = clientes.buscarPorIndice(indice);
+        if (clienteEncontrado != null) {
+            return clienteEncontrado;
+        }
+        return null;
+    }
 
-    // public void recorrerLista() {
-    // Nodo<Cliente> actual = clientes.getHead();
-    // for (int i = 0; i < clientes.getTamano(); i++) {
-    // Cliente clienteActual = actual.getDato();
-    // System.out.println("Indice: " + i + " Cliente: " +
-    // clienteActual.getNombre());
-    // actual = actual.getSiguiente();
-    // }
-    // }
+    public void recorrerLista() {
+        Nodo<Cliente> actual = clientes.getHead();
+        for (int i = 0; i < clientes.getTamano(); i++) {
+            Cliente clienteActual = actual.getDato();
+            System.out.println("Indice: " + i + " Cliente: " +
+                    clienteActual.getNombre());
+            actual = actual.getSiguiente();
+        }
+    }
+
+    public void actualizarNombreCliente(Cliente cliente, String nuevoNombre) {
+        if (cliente != null) {
+            cliente.setNombre(nuevoNombre);
+            System.out.println("Nombre del cliente actualizado exitosamente.");
+        } else {
+            System.out.println("Cliente no encontrado.");
+        }
+    }
+
+    public void actualizarTelefonoCliente(Cliente cliente, String nuevoTelefono) {
+        if (cliente != null) {
+            cliente.setTelefono(nuevoTelefono);
+            System.out.println("Teléfono del cliente actualizado exitosamente.");
+        } else {
+            System.out.println("Cliente no encontrado.");
+        }
+    }
+
+    public void actualizarDireccionCliente(Cliente cliente, String nuevaDireccion) {
+        if (cliente != null) {
+            cliente.setDireccion(nuevaDireccion);
+            System.out.println("Dirección del cliente actualizada exitosamente.");
+        } else {
+            System.out.println("Cliente no encontrado.");
+        }
+    }
+
+    public boolean eliminarCliente(Cliente cliente) {
+        if (cliente != null) {
+            boolean eliminado = clientes.eliminar(cliente);
+            if (eliminado) {
+                System.out.println("Cliente eliminado exitosamente.");
+                return true;
+            } else {
+                System.out.println("Error al eliminar el cliente.");
+                return false;
+            }
+        } else {
+            System.out.println("Cliente no encontrado.");
+            return false;
+        }
+    }
 
 }
