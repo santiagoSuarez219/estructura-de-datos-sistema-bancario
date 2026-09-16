7704bcc844296e03e38bd81fcd80e8ba6f5febad
santiagoSuarez219 <santiago8628@gmail.com>
2026-09-16 11:46:53 -0500

    reordena opciones del menú y mejora la funcionalidad de búsqueda y eliminación de clientes

--- src/com/itm/sistemabancario/view/MenuView.java
+++ src/com/itm/sistemabancario/view/MenuView.java
@@ -15,9 +15,11 @@ public class MenuView {
             opcion = ConsoleUtils.leerEntero("Seleccione una opcion: ");
             switch (opcion) {
                 case 1 -> crearCliente();
-                case 2 -> buscarClientePorIdentificacion();
-                case 3 -> buscarClientePorIndice();
-                case 4 -> recorrerClientes();
+                case 2 -> buscarClientePorIndice();
+                case 3 -> buscarClientePorIdentificacion();
+                case 4 -> actualizarClientePorIdentificacion();
+                case 5 -> eliminarClientePorIdentificacion();
+                case 6 -> listarClientes();
                 case 0 -> System.out.println("Saliendo del sistema...");
                 default -> System.out.println("Opcion invalida.");
             }
@@ -28,8 +30,11 @@ public class MenuView {
 
     private void mostrarMenu() {
         System.out.println("1. Crear cliente");
-        System.out.println("2. Crear cuenta para un cliente");
-        System.out.println("3. Buscar cuentas de un cliente");
+        System.out.println("2. Buscar cliente por indice");
+        System.out.println("3. Buscar cliente por identificacion");
+        System.out.println("4. Actualizar cliente");
+        System.out.println("5. Eliminar un cliente por identificacion");
+        System.out.println("6. Listar clientes en el sistema");
         System.out.println("0. Salir");
     }
 
@@ -38,17 +43,23 @@ public class MenuView {
         String nombre = ConsoleUtils.leerTexto("Nombre: ");
         String telefono = ConsoleUtils.leerTexto("Telefono: ");
         String direccion = ConsoleUtils.leerTexto("Direccion: ");
-
         clienteService.crearCliente(identificacion, nombre, telefono, direccion);
         System.out.println("Cliente creado exitosamente.");
     }
 
-    private Cliente buscarClientePorIdentificacion() {
-        String identificacion = ConsoleUtils.leerTexto("Identificacion del cliente: ");
-        return clienteService.buscarPorIdentificacion(identificacion);
-    }
+    // private Cliente buscarClientePorIdentificacion() {
+    // String identificacion = ConsoleUtils.leerTexto("Identificacion del cliente:
+    // ");
+    // }
 
-    private Cliente buscarPorIndice() {
+    private void buscarClientePorIndice() {
+        int indice = ConsoleUtils.leerEntero("Ingrese el indice del cliente: ");
+        Cliente clienteEncontrado = clienteService.buscarPorIndice(indice);
+        if (clienteEncontrado == null) {
+            System.out.println("Cliente no encontrado");
+        } else {
+            System.out.println("Cliente encontrado: " + clienteEncontrado.getNombre());
 
+        }
     }
 }
